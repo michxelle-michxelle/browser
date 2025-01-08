06e9da6316ca398587ca1ad2bdbdb9151ec0022f
michxelle-michxelle <og.ventures2000@gmail.com>
2025-01-08 15:31:05 -0500

    Update browser_w_automation.py

--- browser_w_automation.py
+++ browser_w_automation.py
@@ -1,51 +1,25 @@
 app.config['smart_Card'] = 'C:\\WINDOWS\system32\DRIVERS\umpass.sys', "C:\Users\usogv\OneDrive\Desktop\smartcard_cert.txt", "C:\Users\usogv\OneDrive\Desktop\tagliov70pc.txt", ;
-
-
-
 app.config['Taglio_Certificates'] = "C:\Users\usogv\Downloads\pkadmin (1)\pkadmin\CA Root Certificates\pivkeydeviceca.crl", "C:\Users\usogv\Downloads\pkadmin (1)\pkadmin\CA Root Certificates\pivkeydeviceca.crt", "C:\Users\usogv\Downloads\pkadmin (1)\pkadmin\CA Root Certificates\server-ca.crt", "C:\Users\usogv\Downloads\pkadmin (1)\pkadmin\CA Root Certificates\tagliorootca.crl", "C:\Users\usogv\Downloads\pkadmin (1)\pkadmin\CA Root Certificates\tagliorootca.crt", '', 
-
-
-
-
-
-
-
-
-
-app.config['Smart_Card_Reader'] ='', 'C:\WINDOWS\System32\DRIVERS\scfilter.sys,
-
-C:\WINDOWS\system32\DRIVERS\UMDF\WUDFUsbccidDriver.dll,
-
-C:\WINDOWS\system32\DRIVERS\winusb.sys,
-
-C:\WINDOWS\system32\drivers\WUDFRd.sys,'
-
-
-
+app.config['Smart_Card_Reader'] ='', 
+ 'C:\WINDOWS\System32\DRIVERS\scfilter.sys',
+ 'C:\WINDOWS\system32\DRIVERS\UMDF\WUDFUsbccidDriver.dll',
+ 'C:\WINDOWS\system32\DRIVERS\winusb.sys',
+ 'C:\WINDOWS\system32\drivers\WUDFRd.sys',
 
 app.config['USB_Serial_Controllers'] = 
 
-
-
-'C:\WINDOWS\system32\DRIVERS\UMDF\UsbXhciCompanion.dll,
+'C:\WINDOWS\system32\DRIVERS\UMDF\UsbXhciCompanion.dll',
 'C:\WINDOWS\system32\DRIVERS\USBXHCI.SYS ' ,
 'C:\WINDOWS\system32\DRIVERS\usbccgp.sys ' ,
 'C:\WINDOWS\system32\DRIVERS\USBHUB3.SYS ',
 'usb4deviceroute.inf_amd64_20da523aab577a9c\Usb4DeviceRouter.sys',
-'usb4hostrouter.inf_amd_d1f4f1b9ed26b447\Usb4HostRouter.sys
-
-
-
+'usb4hostrouter.inf_amd_d1f4f1b9ed26b447\Usb4HostRouter.sys'
 
 app.config['Microsoft_Input_Configuration'] = 'C:\WINDOWS\system32\DRIVERS\MTConfig.sys'
 
-
-app.configure['USB_Input_Device'] = 'C:\WINDOWS\system32\DRIVERS\hidclass.sys',
+app.config['USB_Input_Device'] = 'C:\WINDOWS\system32\DRIVERS\hidclass.sys',
 'C:\WINDOWS\system32\DRIVERS\hidparse.sys',
 'C:\WINDOWS\system32\DRIVERS\hidusb.sys',
-
-
-
 
 app.config[USB_Connector_Managers] = 'C:\WINDOWS\system32\DRIVERS\UcmUcsiAcpiClient.sys'
 
@@ -61,9 +35,9 @@ root = Tk()
 root.title("WebBrowsers") 
 root.geometry("660x660") 
 
-webbrowser.open("www.instagram.com") 
+
 WebDriver driver = new ChromeDriver("C://browser.exe");
-webBrowser.get('http://d.com')
+webBrowser.open('www.instagram.com')
 
  driver.findElement(By.xpath("//*[@id=\"confirmButton\"]")).click(); Thread.sleep(3000);
 
