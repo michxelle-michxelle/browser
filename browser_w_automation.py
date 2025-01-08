e5fafabb49868ffb9c1d6db09ea469161c860002
michxelle-michxelle <og.ventures2000@gmail.com>
2025-01-08 15:20:21 -0500

    Update browser_w_automation.py

--- browser_w_automation.py
+++ browser_w_automation.py
@@ -1,39 +1,20 @@
 
 root = Tk() 
-  
- 
 root.title("WebBrowsers") 
-  
- 
 root.geometry("660x660") 
-  
- 
+
 webbrowser.open("www.instagram.com") 
-
-
-
-
 WebDriver driver = new ChromeDriver("C://browser.exe");
-
 webBrowser.get('http://d.com')
 
-
-
  driver.findElement(By.xpath("//*[@id=\"confirmButton\"]")).click(); Thread.sleep(3000);
-
-
 
 
 //DBS PROMPT
 
     Alert alert = driver.switchTo().alert();  alert.accept();
 
-
         driver.findElement(By.id("dbs")).sendKeys("000000);
-
-        
-       
-
         driver.findElement(By.className("dbs-button")).click(); Thread.sleep(3000);
     }
 
@@ -46,21 +27,8 @@ webBrowser.get('http://d.com')
 
 
 
-
-
-
-
-
-
-/// LOGIN
-
-
-
-
-      
-        driver.findElement(By.id("luser")).sendKeys("xyz@gmail.com");
-     
+/// LOGIN     
+        driver.findElement(By.id("luser")).sendKeys("xyz@gmail.com"); 
         driver.findElement(By.id("password")).sendKeys("xyz12345");
-
         driver.findElement(By.className("signin-button")).click();
     }
